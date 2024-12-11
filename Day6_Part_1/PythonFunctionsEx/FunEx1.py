456a5f32d7214ad5e159cbfafb976085e5c158d2
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2024-12-11 10:33:34 +0530

    code

---
+++ Day6_Part_1/PythonFunctionsEx/FunEx1.py
@@ -0,0 +1,12 @@
+def sayHello():
+    print("Welcome to Python Programming...")
+
+def topic():
+    print("Topic is Python Functions concept is going on")
+
+def company():
+    print("Company is Wipro")
+    
+sayHello()
+topic()
+company()
---
+++ Day6_Part_1/PythonFunctionsEx/FunEx3.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    return x+y
+
+def sub(x, y):
+    return x-y
+
+def mult(x, y):
+    return x * y
+
+a=int(input("Enter First Number   "))
+b=int(input("Enter Second Number   "))
+print("Sum is  ", add(a, b))
+print("Sub is  ", sub(a, b))
+print("Mult is  ", mult(a,b))
